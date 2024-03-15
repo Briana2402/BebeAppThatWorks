b182a070702ca0b7dad8cab47382edb25e209f89
Briana2402 <brianaisaila@gmail.com>
2024-03-15 01:30:17 +0100

    Login and Reset Password

--- app/src/main/java/com/example/bebeappthatworks/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/bebeappthatworks/ui/login/LoginActivity.java
@@ -1,35 +1,21 @@
 package com.example.bebeappthatworks.ui.login;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.bebeappthatworks.MainActivity;
 import com.example.bebeappthatworks.R;
-import com.example.bebeappthatworks.ui.login.LoginViewModel;
-import com.example.bebeappthatworks.ui.login.LoginViewModelFactory;
+import com.example.bebeappthatworks.forgotPassword.ForgotPasswordActivity;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -40,22 +26,36 @@ public class LoginActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Button backButton;
+        Button backToMain;
+        Button backToLogin;
+        TextView forgotPassword;
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        //getSupportActionBar().hide();
 
         setContentView(R.layout.activity_login);
 
-        backButton = (Button) findViewById(R.id.BackToMain); //back button
+        backToMain = (Button) findViewById(R.id.backToMain); //back button
+        forgotPassword = (TextView) findViewById(R.id.forgotPassword); //back button
 
-        backButton.setOnClickListener(new View.OnClickListener() {
+        backToMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
