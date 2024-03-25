7817f05f1b175a72b661224612e407bab8ea3487
Briana2402 <brianaisaila@gmail.com>
2024-03-25 15:51:56 +0100

    small pretty changes

--- app/src/main/java/com/example/bebeappthatworks/EventsFragment.java
+++ app/src/main/java/com/example/bebeappthatworks/EventsFragment.java
@@ -1,15 +1,22 @@
 package com.example.bebeappthatworks;
 
 import android.content.Context;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SearchView;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -21,7 +28,8 @@ import com.example.bebeappthatworks.placeholder.PlaceholderContent;
  */
 public class EventsFragment extends Fragment {
 
-    // TODO: Customize parameter argument names
+    private MenuItem menuItem;
+    private SearchView searchView;
     private static final String ARG_COLUMN_COUNT = "column-count";
     // TODO: Customize parameters
     private int mColumnCount = 1;
@@ -50,12 +58,14 @@ public class EventsFragment extends Fragment {
         if (getArguments() != null) {
             mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_events_list, container, false);
+
 
         // Set the adapter
         if (view instanceof RecyclerView) {
