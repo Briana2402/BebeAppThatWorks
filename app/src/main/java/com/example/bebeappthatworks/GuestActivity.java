2ca37b71358772969f3f425e62b7b6a48b6ac645
MaraB3112 <maraburghelea06@gmail.comm>
2024-03-31 17:26:28 +0300

    User deletion and account created by user deletion + merge with alexia. ??

---
+++ app/src/main/java/com/example/bebeappthatworks/GuestActivity.java
@@ -0,0 +1,22 @@
+//package com.example.bebeappthatworks;
+//
+//import android.os.Bundle;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.fragment.app.Fragment;
+//import androidx.fragment.app.FragmentManager;
+//import androidx.fragment.app.FragmentTransaction;
+//
+//import com.example.bebeappthatworks.databinding.OrganiserLayoutBinding;
+//
+//public class GuestActivity extends AppCompatActivity {
+//
+//
+//    @Override
+////    protected void onCreate(Bundle savedInstanceState) {
+////        super.onCreate(savedInstanceState);
+////        setContentView(new GuestFragment());
+////
+//
+//    }
+//}
