dc0470de060d70bf2059f38e1293e2ec845a68e8
ana <a.sirbu@student.tue.nl>
2024-03-31 16:13:26 +0200

    Display one event separately.

--- app/src/main/java/com/example/bebeappthatworks/AttendeeActivity.java
+++ app/src/main/java/com/example/bebeappthatworks/AttendeeActivity.java
@@ -1,27 +1,17 @@
 package com.example.bebeappthatworks;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.Toast;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.bebeappthatworks.databinding.AttendeeLayoutBinding;
-import com.example.bebeappthatworks.ui.eventCreation.Event;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class AttendeeActivity extends AppCompatActivity {
 
@@ -31,11 +21,16 @@ public class AttendeeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         binding = AttendeeLayoutBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         EventsFragment fragment = new EventsFragment();
         replaceFragment(fragment);
+
 
         binding.bottomNavView.setOnItemSelectedListener(item -> {
             int id = item.getItemId();
@@ -53,13 +48,21 @@ public class AttendeeActivity extends AppCompatActivity {
             return true;
         });
 
+
     }
 
-    private void replaceFragment(Fragment fragment) {
+    public void buttonPlease(View v) {
+        Intent intent = new Intent(AttendeeActivity.this, OneEventActivity.class);
+        startActivity(intent);
+    }
+
+
+    public void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.navigation_host_fragment_content_main, fragment);
         fragmentTransaction.commit();
 
     }
+
 }
--- app/src/main/java/com/example/bebeappthatworks/OneEventFragment.java
+++ app/src/main/java/com/example/bebeappthatworks/OneEventFragment.java
@@ -1,19 +1,14 @@
 package com.example.bebeappthatworks;
 
-import static com.google.android.gms.tasks.Tasks.await;
-
 import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.bebeappthatworks.ui.eventCreation.Event;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,33 +16,20 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.example.bebeappthatworks.OneEventActivity;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A fragment representing a list of Items.
- */
-public class EventsFragment extends Fragment {
-
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
+public class OneEventFragment extends Fragment {
 
     public final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public List<Event> allEvents = new ArrayList<>();
-
+    public List<Event> theEvent = new ArrayList<>();
 
     View view;
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
-    public EventsFragment() {
+    public OneEventFragment() {
     }
 
 
@@ -60,9 +42,8 @@ public class EventsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_events_list, container, false);
+        view = inflater.inflate(R.layout.fragment_one_event, container, false);
 
-        // Set the adapter
         db.collection("Events")
                 .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -71,15 +52,18 @@ public class EventsFragment extends Fragment {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 allEvents.add(document.toObject(Event.class));
                             }
+                            theEvent.add(allEvents.get(0));
                             if (view instanceof RecyclerView) {
-                                Context context = view.getContext();
                                 RecyclerView recyclerView = (RecyclerView) view;
-                                recyclerView.setAdapter(new MyItemRecyclerViewAdapter(allEvents));
+                                recyclerView.setAdapter(new OneEventRecyclerView(theEvent));
                             }
 
                         }
                     }
                 });
+
         return view;
     }
+
+
 }
