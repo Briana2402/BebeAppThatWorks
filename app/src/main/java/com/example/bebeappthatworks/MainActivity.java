737e6d2fdb625b805124cdd45b2f9b5c7637a624
MaraB3112 <maraburghelea06@gmail.comm>
2024-03-31 17:27:03 +0300

    Revert "User deletion and account created by user deletion + merge with alexia. ??"
    This reverts commit 9d80997183856460fabe32055b9d05d00259f5a1.

--- app/src/main/java/com/example/bebeappthatworks/MainActivity.java
+++ app/src/main/java/com/example/bebeappthatworks/MainActivity.java
@@ -107,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
         guestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                Intent intent = new Intent(MainActivity.this, EventCreationActivity.class);
                 startActivity(intent);
             }
         });
