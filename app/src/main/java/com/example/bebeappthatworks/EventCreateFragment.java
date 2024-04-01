753051505f1cc6875c5e1238ba69b4ba60482465
Malinutz <m.corduneanu@student.tue.nl>
2024-04-01 10:57:31 +0200

    Working on Calendar.

--- app/src/main/java/com/example/bebeappthatworks/EventCreateFragment.java
+++ app/src/main/java/com/example/bebeappthatworks/EventCreateFragment.java
@@ -1,5 +1,6 @@
 package com.example.bebeappthatworks;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -10,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -51,6 +53,9 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import com.example.bebeappthatworks.R;
 import com.example.bebeappthatworks.forgotPassword.ForgotPasswordActivity;
@@ -73,6 +78,8 @@ public class EventCreateFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    final Calendar myCalendar = Calendar.getInstance();
 
     View view;
 
@@ -160,6 +167,30 @@ public class EventCreateFragment extends Fragment {
         captureCoverBtn = view.findViewById(R.id.button_capture);
         CheckBox paidEvent = (CheckBox) view.findViewById(R.id.checkBox);
 
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, day);
+                updateLabel();
+            }
+
+            private void updateLabel() {
+                String myFormat="dd/MM/yy";
+                SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.ENGLISH);
+                eventDateEdt.setText(dateFormat.format(myCalendar.getTime()));
+            }
+        };
+
+        eventDateEdt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(v.getContext(), date, myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH));
+            }
+        });
+
         paidEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -184,7 +215,7 @@ public class EventCreateFragment extends Fragment {
                 // getting data from edittext fields.
                 eventName = eventNameEdt.getText().toString();
                 eventDescription = eventDescriptionEdt.getText().toString();
-                eventDate = eventDateEdt.getText().toString();
+                eventDate = eventDateEdt.toString();
                 eventDuration = eventDurationEdt.getText().toString();
                 eventLocation = eventLocationEdt.getText().toString();
                 eventCapacity = eventCapacityEdt.getText().toString();
