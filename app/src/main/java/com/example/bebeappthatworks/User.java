518e794e78b6ebf93cfd417e675301149bb25b1f
Briana2402 <brianaisaila@gmail.com>
2024-04-02 04:27:24 +0200

    YAY WE BALL IT WORKS (ISH)

--- app/src/main/java/com/example/bebeappthatworks/User.java
+++ app/src/main/java/com/example/bebeappthatworks/User.java
@@ -1,7 +1,7 @@
 package com.example.bebeappthatworks;
 
 public class User {
-    public String email, role;
+    public String email, role, name;
 
     public User() {
         // empty constructor
@@ -13,4 +13,23 @@ public class User {
         this.role = role;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
