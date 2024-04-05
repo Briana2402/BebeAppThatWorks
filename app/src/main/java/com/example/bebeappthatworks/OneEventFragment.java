bb4eb3d4763e202bc118a328797fd19ae511e607
ana <a.sirbu@student.tue.nl>
2024-04-05 23:44:45 +0200

    All us united!

--- app/src/main/java/com/example/bebeappthatworks/OneEventFragment.java
+++ app/src/main/java/com/example/bebeappthatworks/OneEventFragment.java
@@ -1,8 +1,6 @@
 package com.example.bebeappthatworks;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,11 +23,15 @@ public class OneEventFragment extends Fragment {
 
     public final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+    // array list to be used when fetching all events
     public List<Event> allEvents = new ArrayList<>();
+
+    // additional array list to be used for displaying one event using the same recyclerview
     public List<Event> theEvent = new ArrayList<>();
 
     View view;
 
+    // empty constructor
     public OneEventFragment() {
     }
 
@@ -40,11 +42,22 @@ public class OneEventFragment extends Fragment {
 
     }
 
+    /**
+     * Creates the view for the first event fetched and sets it
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to.  The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return @view
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_one_event, container, false);
-        Log.d("ma ta","fara liniuta");
 
         db.collection("Events")
                 .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
